3a6a7ab63b8fd7efc5933ef3efe98aaccea3e1b9
Trey Chapman <trevorlchapman@gmail.com>
2017-02-24 14:31:24 -0500

    new scss build

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
+import { environment } from '../../environments/environment';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,14 +12,14 @@ export class UserService{
   constructor(private http: Http) { }
 
   getUser(){
-  	return this.http.get('http://localhost:3000/repos', {})
+  	return this.http.get(`${environment.apiBaseUrl}/repos`, {})
   		.map((res:Response) => {
   			return res.json(); 
   		});
   }
 
   public getOrgs():any{
-    return this.http.get('http://localhost:3000/orgs', {})
+    return this.http.get(`${environment.apiBaseUrl}/orgs`, {})
       .map((res:Response) => {
         return res.json(); 
       });
@@ -25,7 +27,7 @@ export class UserService{
 
   archive(repo: string): Observable<void> {
     return this.http.post(
-        'http://localhost:3000/archive',
+        `${environment.apiBaseUrl}/archive`,
         {repo: repo})
         .map((response: Response) => {
            console.log(response)
